c6b1eae1aeb6a8342d4061dfb4601fe294e01f18
George Torop <gotorop92@gmail.com>
2023-11-29 21:49:09 +0300

    initial

---
+++ src/main/java/com/torop/artisan/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.torop.artisan.repository;
+
+import com.torop.artisan.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}

51a51e4a57a6c03cb9bead0363ae4fe0658e3a7f
George Torop <gotorop92@gmail.com>
2024-04-20 15:03:52 +0300

    created entity for favorite

---
+++ src/main/java/com/torop/artisan/model/Favorite.java
@@ -0,0 +1,50 @@
+package com.torop.artisan.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "favorite_products")
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Favorite(User user, Product product) {
+        this.user = user;
+        this.product = product;
+    }
+
+    public Favorite() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
